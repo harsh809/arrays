84f20ba8f5ae411c95cbc88ad8cc53a673d7da47
harsh809 <121225146+harsh809@users.noreply.github.com>
2022-12-27 15:43:29 +0530

    Add files via upload

---
+++ binarysearchII#23.cpp
@@ -0,0 +1,31 @@
+/*Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ */
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int rowindex = 0;
+        int colindex = col-1;
+        while(rowindex<row && colindex>=0){
+            int temp = matrix[rowindex][colindex];
+            if(temp==target){
+                return 1;
+            }
+            else if(temp>target){
+                colindex--;
+            }
+            else{
+                rowindex++;
+            }
+        }
+        return 0;
+    }
+};
+
+
+
